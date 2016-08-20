2ae3277c63d2195789abb849cd0184666f761dc4
Aporter53 <pavelow53.andrew@gmail.com>
2016-08-20 17:29:53 -0400

    Formated Label Output
    Added .format to the label output. This fixed the issue I was having
    when a stock had no dividend and the program was attempting to output an
    integer as 'None'.

--- runAnalytics.py
+++ runAnalytics.py
@@ -14,14 +14,13 @@ def run(input1):
 
     if ticker.get_dividend_yield() is None:
         ticker.get_dividend_yield == 0
-
-    share_price    = Label(loadAnalytics, text = "Share Price: " + ticker.get_price()).pack()
-    prev_open      = Label(loadAnalytics, text = "Previous Open: " + ticker.get_open()).pack()
-    prev_close     = Label(loadAnalytics, text = "Previous CLose: " + ticker.get_prev_close()).pack()
-    #dividend_yield = Label(loadAnalytics, text = "Dividend Yield: " + ticker.get_dividend_yield()).pack()
-    year_low       = Label(loadAnalytics, text = "52 Week Low: " + ticker.get_year_low()).pack()
-    year_high      = Label(loadAnalytics, text = "52 Week High: " + ticker.get_year_high()).pack()
-    volume         = Label(loadAnalytics, text = "Volume: " + ticker.get_volume()).pack()
+    share_price    = Label(loadAnalytics,text='Share Price: {}'.format(ticker.get_price())).pack()
+    prev_open      = Label(loadAnalytics,text='Previous Open: {}'.format(ticker.get_open())).pack()
+    prev_close     = Label(loadAnalytics,text='Previous CLose: {}'.format(ticker.get_prev_close())).pack()
+    dividend_yield = Label(loadAnalytics,text='Dividend Yield: {}'.format(ticker.get_dividend_yield())).pack()
+    year_low       = Label(loadAnalytics,text='52 Week Low: {}'.format(ticker.get_year_low())).pack()
+    year_high      = Label(loadAnalytics,text='52 Week High: {}'.format(ticker.get_year_high())).pack()
+    volume         = Label(loadAnalytics,text='Volume: {}'.format(ticker.get_volume())).pack()
 
     previous_gain            = format(float(ticker.get_open()) - float(ticker.get_prev_close()), '.2f')
     previous_gain_percentage = format((float((previous_gain))/float((ticker.get_prev_close())))*100, '.2f')
